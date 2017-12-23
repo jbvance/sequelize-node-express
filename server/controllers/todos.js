1596dce9c7a180fe905e8c36419f097c82bd3444
Jason Vance <jbvance@gmail.com>
2017-12-23 15:57:10 -0600

    implement update todo functionality

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -21,6 +21,7 @@ module.exports = {
       .then(todos => res.status(200).send(todos))
       .catch(error => res.status(400).send(error));
   },
+  
   retrieve(req, res) {
     return Todo
       .findById(req.params.todoId, {
@@ -38,5 +39,29 @@ module.exports = {
         return res.status(200).send(todo);
       })
       .catch(error => res.status(400).send(error));
+  },
+
+  update(req, res) {
+    return Todo
+      .findById(req.params.todoId, {
+        include: [{
+          model: TodoItem,
+          as: 'todoItems',
+        }],
+      })
+      .then(todo => {
+        if (!todo) {
+          return res.status(404).send({
+            message: 'Todo Not Found',
+          });
+        }
+        return todo
+          .update({
+            title: req.body.title || todo.title,
+          })
+          .then(() => res.status(200).send(todo))  // Send back the updated todo.
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
   },
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,9 +6,11 @@ module.exports = (app) => {
     message: 'Welcome to the Todos API!',
   }));
 
+  app.put('/api/todos/:todoId', todosController.update);
   app.post('/api/todos', todosController.create);
   app.get('/api/todos', todosController.list);
-  app.get('/api/todos/:todoId', todosController.retrieve)
+  app.get('/api/todos/:todoId', todosController.retrieve);
+ 
 
   app.post('/api/todos/:todoId/items', todoItemsController.create)
 };
